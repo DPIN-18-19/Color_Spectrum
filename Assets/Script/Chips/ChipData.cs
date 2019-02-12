d9ed820ce4a2b59b93a09eb3f9b3c5e3536865f7
GonCas11 <gonzalo.castiella@gmail.com>
2019-02-12 16:08:44 +0100

    Created "Store" scene. Made a visual store skeleton. Added "Price" data to chip data. Created StoreChip prefab. Store loads all store chips upon entering. Chips display the price entered in chip data.

--- Assets/Script/Chips/ChipData.cs
+++ Assets/Script/Chips/ChipData.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 [System.Serializable]
 public class ChipData
 {
+    // Datos del chips
     public string id;                      // Identificador
     public string name;                    // Nombre de chip
     public float weight;
     public List<BonusStat> player_stats;   // Estadisticas nuevas jugador
     public List<BonusStat> weapon_stats;   // Estadisticas nuevas arma
-
-    //public string ability = "";
-
+    
+    // Datos de inventario
     public bool equipped;
+
+    // Datos de tienda
+    public float price;                     // Campo de tienda
 }
 
---
+++ Assets/Script/Store/SChipData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SChipData : MonoBehaviour
+{
+    public enum ChipType
+    {
+        Upgrade,        // Normal
+        Weapon,         // Chip de arma
+        Ability         // Chip de habilidad
+    }
+    public ChipType chip_type;      // Clase de chip
+    public ChipData data;
+
+    TextMeshProUGUI price;
+
+    public GameObject hover_tooltip;                // Objeto con el que se creará el tooltip
+    GameObject my_hover_tooltip;                    // Referencia al tooltip creado
+
+    protected Transform canvas;                     // El canvas
+
+    private void Start()
+    {
+        canvas = GetComponentInParent<Canvas>().transform;          // Coger el canvas de la interfaz
+        price = transform.Find("PricePanel").GetComponentInChildren<TextMeshProUGUI>();
+        WritePrice();
+    }
+
+    void WritePrice()
+    {
+        price.text = data.price.ToString();
+    }
+
+    //public void OnPointerEnter(PointerEventData p_event_data)
+    //{
+    //    // Evitar que se muestre un tooltip si ya se agarrando algo
+    //    //if (!Input.GetMouseButton(0))
+    //    //{
+    //    //    my_hover_tooltip = Instantiate(hover_tooltip, p_event_data.position, Quaternion.identity);
+    //    //    my_hover_tooltip.transform.SetParent(canvas);
+    //    //}
+    //}
+
+    //public void OnPointerExit(PointerEventData p_event_data)
+    //{
+    //    //if (my_hover_tooltip)
+    //    //    my_hover_tooltip.GetComponent<HoverTooltip>().Leave();
+    //}
+}
