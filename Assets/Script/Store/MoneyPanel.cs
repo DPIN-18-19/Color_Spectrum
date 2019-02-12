ff65419bdb6340016d0990f7d4f7482ba570fe8b
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-02-12 20:42:11 +0100

    Started implementing money.

---
+++ Assets/Script/Store/MoneyPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyPanel : MonoBehaviour
+{
+    float player_money;
+
+    TextMeshProUGUI money_t;
+
+	// Use this for initialization
+	void Start ()
+    {
+        money_t = transform.Find("MoneyText").GetComponent<TextMeshProUGUI>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
