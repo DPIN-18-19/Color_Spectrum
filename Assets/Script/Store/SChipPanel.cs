2b53cc7d1ea8506d8912b39539d0d42ec3bde024
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-02-21 15:00:07 +0100

    Added comments to SChipPanel.cs

--- Assets/Script/Store/SChipPanel.cs
+++ Assets/Script/Store/SChipPanel.cs
@@ -6,32 +6,32 @@ using UnityEngine.UI;
 public class SChipPanel : MonoBehaviour
 {
     [SerializeField]
-    private ChipList store_chips;
+    private ChipList store_chips;           // Chips en la tienda
     [SerializeField]
-    private ChipList inventory_chips;
+    private ChipList inventory_chips;       // Inventario
 
-    public GameObject schip_mould;
+    public GameObject schip_mould;          // Molde de chip tienda
 
-    Transform display_p;
-    Button buy_b;
-    Transform money_p;
-    Transform info_p;
+    Transform display_p;        // Ventana de chips
+    Button buy_b;               // Botón de compra
+    Transform money_p;          // Ventana de dinero
+    Transform info_p;           // Ventana de información de producto
 
+    // Chip seleccionado
     public ChipData chip_to_buy;
-    public bool select_click = false;
-    public bool allow_click = true;
+
+
+    public bool select_click = false;   // Click de selección. Evitar varias operaciones con el mismo click.
+    public bool allow_click = true;     // Permitir selección/deselección en ciertas áreas
 
     private void Awake()
     {
+        // Inicialización
         display_p = transform.Find("DisplayPanel");
         buy_b = transform.parent.Find("BuyButton").GetComponent<Button>();
         money_p = transform.parent.Find("MoneyPanel");
         info_p = transform.parent.Find("InfoPanel");
         LoadChips();
-    }
-
-    private void Start()
-    {
     }
 
     void LoadChips()
@@ -57,42 +57,52 @@ public class SChipPanel : MonoBehaviour
         if(Input.GetMouseButtonUp(0))
             select_click = false;
 
+        // Actualizar botón de compra
         if (chip_to_buy.id == "")
             buy_b.interactable = false;
         else
             buy_b.interactable = true;
 
+        // Move this to buy function and init
         ItemPurchasable();
     }
 
     public void MakeSelection(ChipData selected)
     {
+        // Guardar datos de selección
         chip_to_buy.Clone(selected);
+        // Mostrar datos en ventana de información
         info_p.GetComponent<InfoPanel>().ShowUpgrade(chip_to_buy);
     }
 
     public void ClearSelect()
     {
+        // Deseleccionar objeto seleccionado
         for (int i = 0; i < display_p.childCount; ++i)
         {
-            Debug.Log(display_p.GetChild(i).GetComponent<SChipData>().data.id);
             if (display_p.GetChild(i).GetComponent<SChipData>().data.id == chip_to_buy.id)
             {
                 display_p.GetChild(i).GetComponent<SChipData>().DeselectChip();
                 chip_to_buy.id = "";
+                break;
             }
         }
 
+        // Cambiar ventana de información
         info_p.GetComponent<InfoPanel>().ShowDialogue();
     }
 
+    // Compra del objeto seleccionado
     public void BuyItem()
     {
         ChipData bought = new ChipData();
-        bought.Clone(chip_to_buy);
+        bought.Clone(chip_to_buy); 
+        // Restar precio a dinero
         money_p.GetComponent<MoneyPanel>().SpendMoney(bought.price);
+        // Incluir objeto en inventario
         inventory_chips.chips.Add(bought);
 
+        // Eliminar objeto físico de tienda
         for(int i = 0; i < display_p.childCount; ++i)
         {
             if(display_p.GetChild(i).GetComponent<SChipData>().data.id == chip_to_buy.id)
@@ -101,12 +111,14 @@ public class SChipPanel : MonoBehaviour
                 break;
             }
         }
-
+        // Eliminar datos de objeto de tienda
         int index = store_chips.chips.FindIndex(x => x.id == chip_to_buy.id);
         store_chips.chips.RemoveAt(index);
+        // Reiniciar selección
         chip_to_buy.id = "";
     }
 
+    // Comprobar si el jugador tiene dinero suficiente para cada objeto.
     void ItemPurchasable()
     {
         for (int i = 0; i < display_p.childCount; ++i)
