bf66c39200cf3ddbde2e3620500bfb59e262f600
JaimeCibran <jaime.cibran1@gmail.com>
2019-03-11 13:49:54 +0100

    Animacion de la torreta implementada y logica en progreso

---
+++ Assets/Script/Enemy/Turret/ShotTurretEnemy.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotTurretEnemy : MonoBehaviour
+{
+    int bullet_color;
+
+    public Material yellow_mat;
+    public Material cyan_mat;
+    public Material magenta_mat;
+
+    //public ParticleSystem Shot_effectYellow;
+    //public ParticleSystem Shot_effectBlue;
+    //public ParticleSystem Shot_effectPink;
+    public GameObject bullet;
+    public Transform FirePos_1;
+    public Transform FirePos_2;
+    //public Transform EffectShot;
+    public float timeBetweenShots_1 = 3;
+    public float TimeShots_1;
+    private float TimeShotsMax_1;
+    public float timeBetweenShots_2 = 3;
+    public float TimeShots_2;
+    private float TimeShotsMax_2;
+    public bool isShooting;
+
+    public AudioClip FXShotEnemy;
+    private AudioSource source;
+
+    Slow_Motion Ralentizar;
+    private float RalentizarDisparos;
+
+    public float bullet_speed = 10;
+    public float bullet_dmg = 5;
+    public float bullet_range = 20;
+    public bool bullet_friend = false;
+
+    public bool random = false;
+
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        RalentizarDisparos = 1f;
+        Ralentizar = GameObject.Find("Player_Naomi").GetComponent<Slow_Motion>();
+        TimeShotsMax_1 = TimeShots_1;
+        timeBetweenShots_1 = TimeShots_1;
+        TimeShotsMax_2 = TimeShots_2;
+        timeBetweenShots_2 = TimeShots_2;
+
+    }
+    void Update()
+    {
+        if (Ralentizar.ActivateAbility == true)
+        {
+            source.pitch = Ability_Time_Manager.Instance.FXRalentizado;
+        }
+        if (Ralentizar.ActivateAbility == false)
+        {
+            source.pitch = 1;
+        }
+
+        timeBetweenShots_1 = timeBetweenShots_1 - Time.deltaTime;
+        timeBetweenShots_2 = timeBetweenShots_2 - Time.deltaTime;
+
+        if (timeBetweenShots_1 < 0 && isShooting == true)
+        {
+            AddaptColor();
+            source.PlayOneShot(FXShotEnemy);
+            // EffectShot.SetActive(true);
+            GameObject bullet_shot = Instantiate(bullet, FirePos_1.position, FirePos_1.rotation);
+            //bullet_shot.GetComponent<BulletController>().AddBulletInfo(bullet_color,10, 5,20,false);   //- Create Gun Variables
+            Vector3 bullet_dir = transform.right;
+
+            bullet_shot.GetComponent<BulletController>().AddBulletInfo(bullet_color, -bullet_speed, bullet_dir, bullet_dmg, bullet_range, bullet_friend);   //- Create Gun Variables
+            // Instantiate(Shell, ShellEjection.position, ShellEjection.rotation);
+            timeBetweenShots_1 = TimeShots_1 * RalentizarDisparos;
+            // Invoke("QuitarEfecto", FlashTime);
+        }
+        if (timeBetweenShots_2 < 0 && isShooting == true)
+        {
+            AddaptColor();
+            source.PlayOneShot(FXShotEnemy);
+            // EffectShot.SetActive(true);
+            GameObject bullet_shot = Instantiate(bullet, FirePos_2.position, FirePos_2.rotation);
+            //bullet_shot.GetComponent<BulletController>().AddBulletInfo(bullet_color,10, 5,20,false);   //- Create Gun Variables
+            Vector3 bullet_dir = transform.right;
+
+            bullet_shot.GetComponent<BulletController>().AddBulletInfo(bullet_color, -bullet_speed, bullet_dir, bullet_dmg, bullet_range, bullet_friend);   //- Create Gun Variables
+            // Instantiate(Shell, ShellEjection.position, ShellEjection.rotation);
+            timeBetweenShots_2 = TimeShots_2 * RalentizarDisparos;
+            // Invoke("QuitarEfecto", FlashTime);
+        }
+        if (Ralentizar.ActivateAbility == true)
+        {
+            RalentizarDisparos = Ability_Time_Manager.Instance.Slow_Enemy_Shoot;
+        }
+        if (Ralentizar.ActivateAbility == false)
+        {
+            RalentizarDisparos = 1f;
+        }
+    }
+    void AddaptColor()
+    {
+        bullet_color = gameObject.GetComponent<TurretController>().GetColor();
+
+        switch (bullet_color)
+        {
+            case 0:
+                if (bullet.GetComponent<Renderer>() != null)
+
+                    bullet.GetComponent<Renderer>().material = yellow_mat;
+
+                if (bullet.GetComponent<TrailRenderer>() != null)
+
+                    bullet.GetComponent<TrailRenderer>().material = yellow_mat;
+
+                //Instantiate(Shot_effectYellow.gameObject, EffectShot.position, EffectShot.rotation);
+
+                //Instantiate(ShellEfectYellow.gameObject, ShellEjection.position, ShellEjection.rotation);
+                break;
+            case 1:
+                if (bullet.GetComponent<Renderer>() != null)
+
+                    bullet.GetComponent<Renderer>().material = cyan_mat;
+
+                if (bullet.GetComponent<TrailRenderer>() != null)
+
+                    bullet.GetComponent<TrailRenderer>().material = cyan_mat;
+
+                //Instantiate(Shot_effectBlue.gameObject, EffectShot.position, EffectShot.rotation);
+
+                //Instantiate(ShellEfectBlue.gameObject, ShellEjection.position, ShellEjection.rotation);
+                break;
+            case 2:
+                if (bullet.GetComponent<Renderer>() != null)
+
+                    bullet.GetComponent<Renderer>().material = magenta_mat;
+
+                if (bullet.GetComponent<TrailRenderer>() != null)
+
+                    bullet.GetComponent<TrailRenderer>().material = magenta_mat;
+
+                //Instantiate(Shot_effectPink.gameObject, EffectShot.position, EffectShot.rotation);
+
+                //Instantiate(ShellEfectPink.gameObject, ShellEjection.position, ShellEjection.rotation);
+
+                break;
+            default:
+                Debug.Log("Hello");
+                break;
+        }
+    }
+
+}
---
+++ Assets/Script/Enemy/Turret/TurretController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretController : MonoBehaviour
+{
+    public Material Blue_Material;
+    public Material Yellow_Material;
+    public Material Pink_Material;
+
+    public Material DamageBlueMaterial;
+    public Material DamageYellowMaterial;
+    public Material DamagePinkMaterial;
+
+    [SerializeField]
+    public List<Renderer> renderersToChangeColor;
+
+
+    public float alert_distance;                // Distance at which the enemy detects the player by getting near
+    public float sight_distance;                // Distance at which the enemy detects the player by sight
+    public enum Colors
+    {
+        Yellow,         // Yellow = 0
+        Cyan,           // Cyan = 1
+        Magenta         // Magenta = 2
+    };
+    public Colors cur_color = Colors.Yellow;
+
+    public int GetColor()
+    {
+        return (int)cur_color;
+    }
+    void EnemyColorData()
+    {
+        if (cur_color == Colors.Yellow)
+        {
+            GetComponent<EnemyHealthController>().ChangeToYellow();
+        }
+        else if (cur_color == Colors.Magenta)
+        {
+            GetComponent<EnemyHealthController>().ChangeToMagenta();
+        }
+        else if (cur_color == Colors.Cyan)
+        {
+            GetComponent<EnemyHealthController>().ChangeToCyan();
+        }
+    }
+
+    public void RestoreChangeToYellow()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            r.material = Yellow_Material;    // Apply player material
+            //Instantiate(Change_effectYellow.gameObject, transform.position, Quaternion.identity);
+            //   Debug.Log("Change to yellow");
+        }
+
+        // gameObject.layer = 8;
+
+        // Yellow Layer
+    }
+    public void RestoreChangeToMagenta()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            // Magenta Layer
+            r.material = Pink_Material;      // Apply player material
+                                             //  Debug.Log("Change to magenta");
+                                             //  Instantiate(Change_effectPink.gameObject, transform.position, Quaternion.identity);
+        }
+
+        //  gameObject.layer = 10;
+
+    }
+    public void RestoreChangeToCyan()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            r.material = Blue_Material;      // Apply player material
+                                             //  Debug.Log("Change to cyan");
+                                             //Instantiate(Change_effectBlue.gameObject, transform.position, Quaternion.identity);
+        }
+
+        //  gameObject.layer = 9;
+
+    }
+
+
+
+
+    public void ChangeToDamageYellow()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            // Magenta Layer
+            r.material = DamageYellowMaterial;      // Apply player material
+                                                    //  Debug.Log("Change to magenta");
+                                                    //  Instantiate(Change_effectPink.gameObject, transform.position, Quaternion.identity);
+        }
+
+        // gameObject.layer = 8;
+
+    }
+    public void ChangeToDamageBlue()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            // Magenta Layer
+            r.material = DamageBlueMaterial;      // Apply player material
+                                                  //  Debug.Log("Change to magenta");
+                                                  //  Instantiate(Change_effectPink.gameObject, transform.position, Quaternion.identity);
+        }
+
+        // gameObject.layer = 9;
+
+    }
+    public void ChangeToDamagePink()
+    {
+        foreach (Renderer r in renderersToChangeColor)
+        {
+            // Magenta Layer
+            r.material = DamagePinkMaterial;      // Apply player material
+                                                  //  Debug.Log("Change to magenta");
+                                                  //  Instantiate(Change_effectPink.gameObject, transform.position, Quaternion.identity);
+        }
+
+        //  gameObject.layer = 10;
+
+    }
+}
