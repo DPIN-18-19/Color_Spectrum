79f8a188d107fb79d533de592967cb2920bec128
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-03-28 14:47:59 +0100

    Started with beacon programming.

---
+++ Assets/Script/Obstacles/Beacon/Beacon.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beacon : MonoBehaviour
+{
+    [Header("Datos baliza")]
+    [SerializeField]
+    string beacon_name;             // Nombre para imprimir en barra de vida
+
+    // Health
+    public float max_health;        // Vida de la baliza
+    float health;
+
+
+    // Invincivility
+    [SerializeField]
+    float invincible_dur;           // Duracion de invencibilidad
+    float invincible_c;             // Contador de invencibilidad
+    bool is_invincible;             // Comprobador de invencibilidad
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (is_invincible)
+            IsInvincible();
+	}
+
+    // Recibir dano
+    void GetDamage(float damage)
+    {
+        // Comprobar estado de invencibilidad
+        if(!is_invincible)
+        {
+            health -= damage;
+            invincible_c = invincible_dur;
+            is_invincible = true;
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        float bar_val = health / max_health;
+    }
+
+    void IsInvincible()
+    {
+        invincible_c -= Time.deltaTime;
+
+        if (invincible_c < 0)
+            is_invincible = false;
+    }
+}
