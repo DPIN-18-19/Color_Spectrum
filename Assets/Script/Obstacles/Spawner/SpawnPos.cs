bb11bde438153905c7a9c92660fa2d11183fc6fc
Gonzalo Castiella <gonzalo.castiella@gmail.com>
2019-04-03 20:44:15 +0200

    Started spawners. Created spawner object and variables.

---
+++ Assets/Script/Obstacles/Spawner/SpawnPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPos : MonoBehaviour
+{
+    bool is_spawning;
+
+    float to_spawn_c;
+    float to_spawn_dur;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnEnemy(Transform n_enemy, Transform n_patrol, Transform n_home)
+    {
+        Transform enemy = Instantiate(n_enemy, transform);
+        Transform patrols = Instantiate(n_patrol);
+        patrols.SetParent(enemy);
+        Transform home = enemy.Find("EnemyHome");
+        home = n_home;
+    }
+}
---
+++ Assets/Script/Obstacles/Spawner/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    bool active;        // El spawner esta activado
+
+    List<Transform> spawnees;
+    List<int> spawn_order;
+
+    float next_spawn_c;
+    float next_spawn_dur;
+
+    List<Transform> spawn_patrols;
+    List<Transform> spawn_homes;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
