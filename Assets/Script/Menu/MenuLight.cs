1e63c016311d3af26f25aaf9f4926852a9cec84b
JaimeCibran <jaime.cibran1@gmail.com>
2019-04-03 20:46:14 +0200

    Avanzando en el selector de menus

---
+++ Assets/Script/Menu/MenuLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class MenuLight : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
+{
+    public GameObject Light;
+    public GameObject GeneralLight;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnPointerEnter(PointerEventData pdata)
+    {
+        Light.SetActive(true);
+        GeneralLight.SetActive(false);
+    }
+    public void OnPointerExit(PointerEventData pdata)
+    {
+        Light.SetActive(false);
+        GeneralLight.SetActive(true);
+    }
+}
